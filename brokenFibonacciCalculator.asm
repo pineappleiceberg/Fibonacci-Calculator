0f0d7a79debee728f592f09276ccf51263a3549f
pineappleiceberg <80722277+pineappleiceberg@users.noreply.github.com>
2022-04-30 02:20:33 -0500

    Create brokenFibonacciCalculator.asm

---
+++ brokenFibonacciCalculator.asm
@@ -0,0 +1,59 @@
+	;from scratch fibonacci calculator in masm
+	;utilizing labels and secondary functions which is not the most efficent but useful for learning
+	;contains a logic bug fixed in the next iteration
+	
+	option casemap:none
+	nl = 10
+	.const
+	ttlStr	byte	'fib sequence', 0
+	fibprint	byte	'next number =%d', nl, 0
+	.data
+	firstint dword 1
+	secondint dword 1
+	fibnumber dword 0
+	maxint dword	317811
+	
+	.code
+	externdef printf:proc
+	public getTitle
+	
+	getTitle proc
+	
+	lea rax, ttlStr
+	ret
+	
+	getTitle endp
+	
+	public asmMain
+	asmMain proc
+	sub rsp, 56
+	
+	
+	
+	mov r14d, maxint
+	jmp nextfibnumber
+	
+	
+	
+	
+	nextfibnumber:
+	mov edx, firstint
+	mov ecx, secondint
+	mov r15, rdx
+	add rdx, rcx
+	mov firstint, edx
+	mov secondint, r15d
+	
+	lea rcx, fibprint
+	call printf
+	cmp r15, r14
+	jl nextfibnumber
+	jge endroutine
+	
+	endroutine:
+	add rsp, 56
+	ret
+	
+	asmMain endp
+	
+	end
